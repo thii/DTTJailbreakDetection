9d3435e670ee934782b7edebea4be70d1d915f74
Thi <thi_dt@yahoo.com>
2014-01-03 17:12:54 +0900

    Initial Commit

---
+++ DTTJailbreakDetection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DTTJailbreakDetection
+//
+//  Created by Thi on 1/3/14.
+//  Copyright (c) 2014 Thi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DTTJailbreakDetection/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DTTJailbreakDetection
+//
+//  Created by Thi on 1/3/14.
+//  Copyright (c) 2014 Thi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
