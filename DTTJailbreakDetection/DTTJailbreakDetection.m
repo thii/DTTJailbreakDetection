ab5c750662494481ba620853e2e60d6299c2860f
Doan Truong Thi <thi_dt@yahoo.com>
2014-09-24 16:02:49 +0900

    More checks...

--- DTTJailbreakDetection/DTTJailbreakDetection.m
+++ DTTJailbreakDetection/DTTJailbreakDetection.m
@@ -12,12 +12,38 @@
 
 - (BOOL)isJailbroken
 {
-    FILE *file = fopen("/bin/ssh", "r");
-    if (file) {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    if ([fileManager fileExistsAtPath:@"/Applications/Cydia.app"]) {
+        return YES;
+    } else if ([fileManager fileExistsAtPath:@"/Library/MobileSubstrate/MobileSubstrate.dylib"]) {
+        return YES;
+    } else if ([fileManager fileExistsAtPath:@"/bin/bash"]) {
+        return YES;
+    } else if ([fileManager fileExistsAtPath:@"/usr/sbin/sshd"]) {
+        return YES;
+    } else if ([fileManager fileExistsAtPath:@"/etc/apt"]) {
+        return YES;
+    } else if ([fileManager fileExistsAtPath:@"/bin/ssh"]) {
         return YES;
     }
     
-    fclose(file);
+    // Check if the app can access outside of its sandbox
+    NSError *error = nil;
+    NSString *string = @".";
+    [string writeToFile:@"/private/jailbreak.txt" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    if (!error) {
+        return YES;
+    } else {
+        [fileManager removeItemAtPath:@"/private/jailbreak.txt" error:nil];
+        return NO;
+    }
+    
+    // Check if the app can open a Cydia's URL scheme
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"cydia://package/com.example.package"]]) {
+        return YES;
+    }
+
     return NO;
 }
 
